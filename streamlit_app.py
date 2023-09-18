0c2399ee92090991c9ad620697bef036f259a861
zain639 <145377163+zain639@users.noreply.github.com>
2023-09-18 22:20:23 +0500

    Update streamlit_app.py
    added table to interface

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ st.text("🥗 Kale, Spinach and Rocket Smoothie")
 st.text("🐔 Hard_Boiled Free Range Egg")
 st.text("🥑🍞 Avocado Toast")
 st.header('🍌🥭 Build Your Own Fruit Smoothie🥝🍇 ')
+
+import pandas as pd
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
