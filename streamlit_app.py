0d0a713cc212c361f0351d32116bd6588782d014
zain639 <145377163+zain639@users.noreply.github.com>
2023-09-18 22:10:51 +0500

    Update streamlit_app.py
    Made edits for app interface

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
-import streamlit
-streamlit.title('My parents new healthy Dinner')
+import streamlit as st
+st.title('My parents new healthy Dinner')
+st.markdown("**Breakfast Menu**")
+st.markdown("Omega 3 & Blueberry oatmeanl")
+st.markdown("Kale, Spinach and Rocket Smoothie")
+st.markdown("Hard_Boiled Free Range Egg")
