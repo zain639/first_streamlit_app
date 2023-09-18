2d3a2a8c7ebd45b351d78b56962febd34d5f1105
zain639 <145377163+zain639@users.noreply.github.com>
2023-09-18 22:26:50 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,7 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie🥝🍇 ')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Let's put a pick list here so they can pick the fruit they want to include 
+st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 st.dataframe(my_fruit_list)
+
