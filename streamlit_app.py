6d9b22b039e7e8f4c2fdff7f81f40bec411cb959
zain639 <145377163+zain639@users.noreply.github.com>
2023-09-18 22:21:45 +0500

    Update streamlit_app.py
    removed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ st.text("🥑🍞 Avocado Toast")
 st.header('🍌🥭 Build Your Own Fruit Smoothie🥝🍇 ')
 
 import pandas as pd
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
