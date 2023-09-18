e0ae330551b5ba47f102dd4487753c31473368b5
zain639 <145377163+zain639@users.noreply.github.com>
2023-09-18 22:15:50 +0500

    Update streamlit_app.py
    added Avocado toast and breakfast visuals

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 st.title('My parents new healthy Dinner')
 st.header("Breakfast Menu")
-st.text("Omega 3 & Blueberry oatmeanl")
-st.text("Kale, Spinach and Rocket Smoothie")
-st.text("Hard_Boiled Free Range Egg")
+st.text("🥣 Omega 3 & Blueberry oatmeanl")
+st.text("🥗 Kale, Spinach and Rocket Smoothie")
+st.text("🐔 Hard_Boiled Free Range Egg")
+st.text("🥑🍞 Avocado Toast")
